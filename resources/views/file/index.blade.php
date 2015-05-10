4b3c4909bdc29bc24b9044adf6443f768738bf08
Julien Houvion <julien.houvion@wengo.com>
2015-05-10 21:29:25 +0200

    aqjout de la configuration elfinder

--- resources/views/file/index.blade.php
+++ resources/views/file/index.blade.php
@@ -1,5 +1,7 @@
 @extends('app')
 
 @section('content')
+    <link href="/assets/css/colorbox.css" rel="stylesheet">
+    <script type="text/javascript" src="/assets/js/jquery.colorbox-min.js"></script>
     <div>Bienvenue dans led ficheirs!!!</div>
 @endsection
