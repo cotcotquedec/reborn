7d44b2f1c7e6361a6fd4ba08c4509549bcc5f2e6
Julien Houvion <julien.houvion@wengo.com>
2015-12-04 14:26:00 +0100

    fix filesystem confioguration

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,7 @@ return [
         ],
         'files' => [
             'driver' => 'files',
+            'root'   => storage_path('files'),
         ],
         'torrent' => [
             'driver' => 'local',
