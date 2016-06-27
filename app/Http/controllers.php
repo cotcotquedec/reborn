ff65766fd93881da68d845cbe7319061c6d782c9
Julien Houvion <julien.houvion@wengo.com>
2016-06-27 15:41:18 +0200

    fix route

--- app/Http/controllers.php
+++ app/Http/controllers.php
@@ -5,7 +5,3 @@ Route::controller('user', 'UserController');
 Route::controller('schedule', 'ScheduleController');
 Route::controller('reference', 'ReferenceController');
 Route::controller('jobmaker', 'JobmakerController');
-
-
-// 2016-06-27 15:32:00 Création automatique de la route : test
-Route::controller('test', 'TestController');
